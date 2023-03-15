3d4a5808f6bba44693cf2630ac5bfe324ba27844
Marc <marcpuertom97@gmail.com>
2023-03-15 17:45:20 +0100

    remove and add cards

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ let title = document.getElementById("title");
 let author = document.getElementById("author");
 let pages = document.getElementById("pages");
 let isRead = document.getElementById("checkbox1");
+let container_cards = document.getElementById("card-container");
 
 button_add.onclick = openBookModal;
 close_btn.onclick = closeModal;
@@ -68,7 +69,6 @@ const getBookFromInput = () => {
 };
 
 function addNewBook(e) {
-  console.log("entra");
   e.preventDefault();
   const newBook = getBookFromInput();
 
@@ -84,8 +84,8 @@ function addNewBook(e) {
   author.value="";
   pages.value="";
   isRead.value="";
-  
-  //updateBooksGrid()
+
+  updateBooksGrid()
 
   closeModal();
 }
@@ -93,3 +93,55 @@ function addNewBook(e) {
 function saveLocal() {
   localStorage.setItem("library", JSON.stringify(library.books));
 }
+
+function deleteLocal(item){
+    localStorage.removeItem(item);
+}
+
+function updateBooksGrid(){
+    //reset 
+    container_cards.innerHTML="";
+
+    for (let book of library.books) {
+        createBookCard(book)
+      }
+}
+
+function createBookCard(book){
+
+    const card = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("h4");
+    const pages = document.createElement("h4");
+    const button_delete = document.createElement("input");
+
+    card.classList.add("book-card");
+    button_delete.type = "button";
+    button_delete.value = "REMOVE";
+    //button_delete.onclick= removeAction;
+
+    if (book.isRead){
+        card.classList.add("read");
+    }
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(button_delete);
+    container_cards.appendChild(card);
+
+    button_delete.addEventListener("click", () => {
+        container_cards.removeChild(card);
+        library.removeBook(title.innerHTML);
+        console.log(title.innerHTML);
+    });
+}
+
+
+
+
+
